50f01f50e444c6ce4090c1c2f94a6ad95484888a
Joff Tiquez <jofftiquez@gmail.com>
2022-03-04 16:16:36 +0800

    feat: add menus constants

---
+++ src/constants/menus.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    title: 'Misson',
+    link: '#mission',
+    extraClasses: 'btn-ghost lg:mr-2',
+  },
+  {
+    title: 'Health Infrastracture',
+    link: '#infra',
+    extraClasses: 'btn-ghost lg:mr-2',
+  },
+  {
+    title: 'NFT',
+    link: '#nft',
+    extraClasses: 'btn-ghost lg:mr-2',
+  },
+  {
+    title: 'Whitepaper',
+    link: 'https://pharmacy.hippocrades.com',
+    extraClasses: 'btn-outline btn-primary lg:mr-2'
+  },
+  {
+    title: 'Demo',
+    link: 'https://hippocrades.org',
+    extraClasses: 'text-white hover:btn-primary hover:text-white'
+  },
+];
