44c685118d3341713625a0b943a280db5bd9f575
Joff Tiquez <jofftiquez@gmail.com>
2022-03-04 16:16:18 +0800

    refactor: add theme override

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,29 @@
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
+	// daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: '#0099cc',
+          secondary: '#424242',
+          accent: '#82b1ff',
+          info: '#2196f3',
+          success: '#7fad33',
+          warning: '#FBBD23',
+          error: '#f75a5f',
+        },
+      },
+    ],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
+
 	theme: {
 		extend: {}
 	},
