536466ebe8845185ff394d887929faeb24996e5b
Joff Tiquez <jofftiquez@gmail.com>
2022-03-11 18:39:16 +0800

    refactor: add new link

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,5 @@ export const LINKS_MAP = {
   demo: 'https://demo.hippocrades.org',
   docs: 'https://docs.hippocrades.org',
   apiDocs: 'https://docs.fleming.hippocrades.org/',
+  deploymentsDemo: 'https://youtu.be/wYp-X022_7Y',
 };
